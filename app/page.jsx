7ad9e9d5d5fd50b6150d1444de18ee96b2f0566e
pyter17 <pietror269@gmail.com>
2025-06-28 17:19:23 +0200

    Create page.jsx
    Page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Chat from '@/components/Chat';
+
+export default function Home() {
+  return (
+    <main className="max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold text-center my-4">👩‍💻 Luna AI</h1>
+      <Chat />
+    </main>
+  );
+}
+
+
+
+
